eef69d7e16bc68044ad9ff5a523063684810d742
Igor Ovsyannikov <igorovsyannikov69@mail.ru>
2020-09-03 22:47:54 +0300

    fix

--- lesson_3/react_lesson/src/Components/hooks/useCount.js
+++ lesson_3/react_lesson/src/Components/hooks/useCount.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
-export function useCount() {
+export function useCount(openItem) {
+    console.log(openItem);
     const [count,setCount] = useState(1);
 
     const onChange = e => setCount(e.target.value);
